7f095255a5c75f115de3bab859a1b2868eee19ea
nix <nixafax@bk.ru>
2023-02-18 21:32:24 +0300

    complete second task

---
+++ ex2.py
@@ -0,0 +1,18 @@
+otv=""
+pl = int(input("Введите номер места: "))
+if pl in range (1, 55):
+    if pl in range(1, 37):
+        otv += "Место в купе "
+        if pl%2==0:
+            otv += "снизу"
+        else:
+            otv += "сверху"
+    else:
+        otv += "Боковое место "
+        if pl%2==0:
+            otv += "снизу"
+        else:
+            otv += "сверху"
+    print(otv)
+else:
+    print("Неверно указано номер места")
